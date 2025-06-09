3b21561543f3a4de4166a6ad79643d4bcce7229e
FELIPE <fmpetracchi@gmail.com>
2025-06-09 16:51:22 -0300

    Examen-Se agregó el driver del sensor DHT11

--- firmware/projects/Examen/main/Examen.c
+++ firmware/projects/Examen/main/Examen.c
@@ -1,8 +1,14 @@
-/*! @mainpage Template
+/*! @mainpage Examen
  *
  * @section genDesc General Description
  *
- * This section describes how the program works.
+ * El sistema está compuesto por un sensor de humedad y temperatura DHT11 y un  sensor analógico de radiación.
+ * 
+ * Por un lado se debe detectar el riesgo de nevada, la cual se da si la húmedad se encuentra por encima del
+ * 85%  y la temperatura entre 0 y 2ºC. Para esto se deben tomar muestras cada 1 segundo y se envían por UART
+ * con el siguiente formato: “Temperatura: 10ºC - Húmedad: 70%”
+ * Si se da la condición de riesgo de nevada se debe indicar el estado encendiendo el led Rojo de la placa,
+ * además del envío de un mensaje por la UART : * “Temperatura: 1ºC - Húmedad: 90% - RIESGO DE NEVADA”
  *
  * <a href=" ">Operation Example</a>
  *
@@ -17,7 +23,7 @@
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | XX/0X/2025 | Document creation		                         |
+ * | 09/06/2025 | Document creation		                         |
  *
  * @author Felipe M. Petracchi (felipe.petracchi@ingenieria.uner.edu.ar)
  *
@@ -26,14 +32,22 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "dht11.h"
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
-
+void initComponentes(void) {
+	LedsInit();
+	// DHT11Init(GPIO); // Si el sensor lo requiere. 
+}
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	initComponentes();
+
 }
 /*==================[end of file]============================================*/
