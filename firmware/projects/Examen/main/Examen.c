976f48a654b7b7757b0d14ce6e16210c9e03b705
FELIPE <fmpetracchi@gmail.com>
2025-06-09 17:17:27 -0300

    Examen-Se creó el marco de lectura de Temperatura y Humedad

--- firmware/projects/Examen/main/Examen.c
+++ firmware/projects/Examen/main/Examen.c
@@ -16,7 +16,7 @@
  *
  * |    Peripheral  |   ESP32   	|
  * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * | 	DHT11 Data	| 	GPIO_4		|
  *
  *
  * @section changelog Changelog
@@ -34,20 +34,49 @@
 #include <stdint.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "timer_mcu.h"
 
 #include "dht11.h"
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
+TaskHandle_t medirTyHTask_handle = NULL; // Identificador de la tarea de medición y envío de datos
+
+float temperatura = 0.0f;
+float humedad = 0.0f;
 
 /*==================[internal functions declaration]=========================*/
 void initComponentes(void) {
 	LedsInit();
-	// DHT11Init(GPIO); // Si el sensor lo requiere. 
+	dht11Init(GPIO_23); // Inicializa el DHT11 en el GPIO 4
+}
+
+void medirTyH(void *param) {
+	vTaskNotifyGiveFromISR(medirTyHTask_handle, pdFALSE);
+}
+
+void medirTyHTask(void *param) {
+	while (true) {
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Espera a que se notifique la tarea
+
+		if (dht11Read(&humedad, &temperatura)) {
+			printf("Temperatura: %.1fºC - Húmedad: %.1f%%\n", temperatura, humedad);
+		}
+	}
 }
 /*==================[external functions definition]==========================*/
 void app_main(void){
 	initComponentes();
 
+	timer_config_t timer_medirTyH_config = {
+		.timer = TIMER_A,		// Timer A
+		.period = 1000 * 1000,	// 1 segundo en microsegundos
+		.func_p = medirTyH,		// Función a ejecutar
+		.param_p = NULL			// Parámetro para la función
+	};
+
+	xTaskCreate(&medirTyHTask, "MEDIRTYH", 1024, NULL, 5, &medirTyHTask_handle);
+
+	TimerStart(timer_medirTyH_config.timer);
 }
 /*==================[end of file]============================================*/
